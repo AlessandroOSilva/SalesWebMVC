e3e5d444c8f3ecbf6e5a6c70fd83ee6b0a35ad0c
AlessandroOSilva <alessandro.wolfera@gmail.com>
2021-01-11 21:38:54 -0300

    Create Controller-Model-View Department

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SaleWebMVC.Models;
+using SaleWebMVC.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
