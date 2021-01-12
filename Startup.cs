7c262c8f090c892f15b3d436ed98c51396ab690e
AlessandroOSilva <alessandro.wolfera@gmail.com>
2021-01-12 19:38:46 -0300

    CRUD scaffold, Departments

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SaleWebMVC.Data;
 
 namespace SaleWebMVC
 {
@@ -24,6 +26,9 @@ namespace SaleWebMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<SaleWebMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SaleWebMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
